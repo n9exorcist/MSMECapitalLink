1368d2050a7b9297e0c2998a275f1ebbeed7e41f
Narayanan Selvaraj <n9exorcist@gmail.com>
2026-06-23 15:18:12 +0530

    reverted the standalone

--- cfo-console/next.config.ts
+++ cfo-console/next.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "standalone", // <--- Add this line
   turbopack: {
     root: path.resolve(__dirname),
   },
